beb93754d641bb917cde502fae6a5bdbf4c06ea7
Srilekha Pakalapati <spakalapati@comply>
2026-06-26 13:20:13 +0530

    changes

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,7 @@
 
 const { Pool } = require('pg');
 const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '../../.env') });
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
 const pool = new Pool(
   process.env.DATABASE_URL
